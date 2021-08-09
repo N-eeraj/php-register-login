b5fbbf6d7e679cabfc8e87dcf2e1af815898378d
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-10 02:46:44 +0530

    added custom stylesheet & bootstrap classes for responsivenes

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        $uname = $_POST["uname"];
+        $uname = trim($_POST["uname"]);
         $pswd = $_POST["pswd"];
 
         $conn = new mysqli("localhost", "root", "", "test");
@@ -12,10 +12,12 @@
         $result = $conn->query($sql);
         $record = $result->fetch_row();
 
-        if(empty($record[0]))
-        $status = "<p class='bg-danger col-4'>User not found</p>";
+        if(empty($uname))
+            $status = "<p class='my-alert alert-danger'>Username can't be empty</p>";
+        else if(empty($record[0]))
+        $status = "<p class='my-alert alert-danger'>User not found</p>";
         else
-            $status = $record[0] == $pswd? "<p class='bg-success col-4'>Login Success</p>": "<p class='bg-danger col-4'>Wrong Password</p>";
+            $status = $record[0] == $pswd? "<p class='my-alert alert-success'>Login Success</p>": "<p class='my-alert alert-danger'>Wrong Password</p>";
     }
 
 ?>
@@ -27,28 +29,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
 </head>
-<body class="container">
+<body class="container bg-primary">
 
-    <?php echo $status ?>
+    <div class="card bg-light col-11 col-sm-8 col-lg-5">
+        <?php echo $status ?>
 
-    <form method="POST" class="col-4">
+        <form method="POST">
 
-        <div class="mb-3">
-            <label for="uname" class="form-label">Username</label>
-            <input type="text" id="uname" name="uname" placeholder="Username" class="form-control">
-        </div>
+            <div class="mb-3">
+                <label for="uname" class="form-label">Username</label>
+                <input type="text" id="uname" name="uname" placeholder="Username" class="form-control">
+            </div>
 
-        <div class="mb-3">
-            <label for="pswd" class="form-label">Password</label>
-            <input type="password" id="pswd" name="pswd" placeholder="Password" class="form-control">
-        </div>
-        
-        <input type="submit" value="Login" class="btn btn-primary">
-        <a href="register.php" class="btn btn-secondary">Register</a>
-    </form>
+            <div class="mb-3">
+                <label for="pswd" class="form-label">Password</label>
+                <input type="password" id="pswd" name="pswd" placeholder="Password" class="form-control">
+            </div>
+            
+            <div class="row justify-content-around">
+                <input type="submit" value="Login" class="btn btn-primary col-5 col-sm-4 col-md-3">
+                <a href="register.php" class="btn btn-secondary col-5 col-sm-4 col-md-3">Register</a>
+            </div>
+        </form>
+    </div>
     
 </body>
 </html>
--- register.php
+++ register.php
@@ -9,11 +9,11 @@
         $pswd2 = trim($_POST["pswd2"]);
 
         if(empty($uname))
-            $status = "<p class='bg-danger col-4'>Username can't be empty</p>";
+            $status = "<p class='my-alert alert-danger'>Username can't be empty</p>";
         else if(strlen($pswd1) < 8)
-            $status = "<p class='bg-danger col-4'>Password should be atleast 8 characters long</p>";
+            $status = "<p class='my-alert alert-danger'>Password should be atleast 8 characters long</p>";
         else if($pswd1 != $pswd2)
-            $status = "<p class='bg-danger col-4'>Passwords not matching</p>";
+            $status = "<p class='my-alert alert-danger'>Passwords not matching</p>";
 
         else
         {
@@ -23,14 +23,14 @@
             $record = $result->fetch_row();
 
             if(!empty($record[0]))
-                $status = "<p class='bg-danger col-4'>Username is already taken</p>";
+                $status = "<p class='my-alert alert-danger'>Username is already taken</p>";
 
             else
             {
                 $sql = "INSERT INTO `phplogin` VALUES('$uname', '$pswd1')";
                 $conn->query($sql);
                 $conn->close();
-                $status = "<p class='bg-success col-4'>Registration Success</p>";
+                $status = "<p class='my-alert alert-success'>Registration Success</p>";
             }
         }
     }
@@ -43,33 +43,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title><!-- CSS only -->
+    <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
 </head>
-<body class="container">
+<body class="container bg-primary">
 
-    <?php echo $status ?>
+    <div class="card bg-light col-11 col-sm-8 col-lg-5">
+        <?php echo $status ?>
 
-    <form method="POST" class="col-4">
+        <form method="POST">
 
-        <div class="mb-3">
-            <label for="uname" class="form-label">Username</label>
-            <input type="text" id="uname" name="uname" placeholder="Username" class="form-control">
-        </div>
+            <div class="mb-3">
+                <label for="uname" class="form-label">Username</label>
+                <input type="text" id="uname" name="uname" placeholder="Username" class="form-control">
+            </div>
 
-        <div class="mb-3">
-            <label for="pswd1" class="form-label">Password</label>
-            <input type="password" id="pswd1" name="pswd1" placeholder="Password" class="form-control">
-        </div>
+            <div class="mb-3">
+                <label for="pswd1" class="form-label">Password</label>
+                <input type="password" id="pswd1" name="pswd1" placeholder="Password" class="form-control">
+            </div>
 
-        <div class="mb-3">
-            <label for="pswd2" class="form-label">Confirm Password</label>
-            <input type="password" id="pswd2" name="pswd2" placeholder="Confirm Password" class="form-control">
-        </div>
-        
-        <input type="submit" value="Sign Up" class="btn btn-primary">
-        <a href="login.php" class="btn btn-secondary">Login</a>
-    </form>
+            <div class="mb-3">
+                <label for="pswd2" class="form-label">Confirm Password</label>
+                <input type="password" id="pswd2" name="pswd2" placeholder="Confirm Password" class="form-control">
+            </div>
+            
+            <div class="row justify-content-around">
+                <input type="submit" value="Sign Up" class="btn btn-primary col-5 col-sm-4 col-md-3">
+                <a href="login.php" class="btn btn-secondary col-5 col-sm-4 col-md-3">Login</a>
+            </div>
+        </form>
+    </div>
     
 </body>
 </html>
